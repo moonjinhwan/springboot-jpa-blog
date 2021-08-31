4016605c3346830b0949affb75c5755264a3fc27
moonjinh <moonjinh123@naver.com>
2021-09-01 02:47:55 +0900

    feat: 삭제기능과 예외페이지 처리

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,13 +3,18 @@ package com.cos.blog.test;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -40,6 +45,26 @@ public class DummyControllerTest {
 		Page<User> page = userRepository.findAll(pageable);
 		List<User> pageList = page.getContent();
 		return pageList;
+	}
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (Exception e) {
+			return "삭제에 실패 했습니다.";
+		}
+		return "삭제되었습니다. "+id;
+	}
+	
+	@Transactional 
+	@PutMapping("/dummy/user/{id}")
+	public void updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("유저가 없습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
 	}
 	
 	@PostMapping("/dummy/join")
