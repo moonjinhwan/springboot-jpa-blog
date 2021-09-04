71ba6bc8e174d01ebe9886d877e6d82e2a3b34d1
moonjinh <moonjinh123@naver.com>
2021-09-05 02:47:21 +0900

    feat: 비밀번호 엄호화

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,13 +15,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.
-			authorizeRequests()
-				.antMatchers("/auth/**")
+			csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/auth/**", "/css/**", "/image/**", "/js/**", "/")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
 				.and()
 			.formLogin()
 				.loginPage("/auth/loginForm");		
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder encodePWD() {
+		return new BCryptPasswordEncoder();
 	}
 }
